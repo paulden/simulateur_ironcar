c303e1129a96635a5d823104be93228dddf75fb0
Paul de Nonancourt <pdenonancourt@octo.com>
2019-01-29 10:06:26 +0100

    Add basic test to control origin_pool output

--- tests/pictures_generation_test.py
+++ tests/pictures_generation_test.py
@@ -53,3 +53,34 @@ def test_it_should_call_povray(subprocess_mock):
     # Then
     povray_is_called = 'povray' in subprocess_args[0]
     assert povray_is_called
+
+
+def test_it_should_initialize_origin_pool_with_enough_numbers():
+    # Given
+    configuration = json.load(open(CONFIG_PATH))
+    configuration['images_curve'] = 1
+    configuration['origin_pool_start'] = 100
+    configuration['origin_pool_end'] = 500
+    configuration['origin_pool_step'] = 2
+    expected_length = (configuration['origin_pool_end'] - configuration['origin_pool_start']) / configuration['origin_pool_step']
+
+    # When
+    generator = PicturesGenerator(configuration)
+    origin_pool = generator.origin_pool
+
+    # Then
+    assert len(origin_pool) == expected_length
+
+
+def test_it_should_initialize_origin_pool_with_image_height():
+    # Given
+    configuration = json.load(open(CONFIG_PATH))
+    configuration['images_curve'] = 1
+
+    # When
+    generator = PicturesGenerator(configuration)
+    origin_pool = generator.origin_pool
+
+    # Then
+    for point in origin_pool:
+        assert point[1] == configuration['image_height']
